76899db49700775a458cb579b6c2dbfa2e4317f6
Raman-734 <raman@generaltechnology.com.np>
2025-11-25 21:17:13 +0545

    frontend setup

---
+++ frontend/app/(protectedPages)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { OverviewStats } from "./_components/overview-stats"
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <OverviewStats />
+    </div>
+  )
+}
+
+export default page
